babbf1698b642cab542196a1e5882f2073bd057a
Jan Bartel <jan.bartel@atino.net>
2016-04-12 10:45:49 +0200

    changed Example

--- Example/VISPERS-Wireframe-Protocol/AppDelegate.swift
+++ Example/VISPERS-Wireframe-Protocol/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import VIPERS_Wireframe_Protocol
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,33 +15,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {         // Override point for customization after application launch.
+        let _ = Wireframe()
+        
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
+class Wireframe : WireframeProtocol{
+    func addRouteString(routeString:String){}
+    func addRouteString(routeString: String, handler:((parameters:[String:AnyObject])->Void)){}
+    func routeURL(URL:NSURL){}
+    func routeURL(URL:NSURL,parameters:[String:AnyObject]?){}
+    func routeURL(URL:NSURL,parameters:[String:AnyObject]?,option:RoutingOptionProtocol){}
+    func controllerFor(URL:NSURL,parameters:[String:AnyObject]?) -> UIViewController{return UIViewController()}
+    func canRoute(URL:NSURL) -> Bool{return true}
+    func canRoute(URL:NSURL,parameters:[String:AnyObject]?) -> Bool{return true}
+    func addControllerProvider(provider: ControllerProviderProtocol){}
+    func removeControllerProvider(provider: ControllerProviderProtocol)-> Bool{return true}
+    func controllerProviders() -> [ControllerProviderProtocol]{return [ControllerProviderProtocol]()}
+    func addRoutingOptionProvider(provider:RoutingOptionProviderProtocol){}
+    func removeRoutingOptionProvider(provider:RoutingOptionProviderProtocol) -> Bool{return true}
+    func routingOptionProviders() -> [RoutingOptionProviderProtocol]{return [RoutingOptionProviderProtocol]()}
+    func addControllerRoutingPresenter(presenter: ControllerRoutingPresenterProtocol){}
+    func removeControllerRoutingPresenter(presenter: ControllerRoutingPresenterProtocol) -> Bool{return true}
+    func controllerRoutingPresenters()->[ControllerRoutingPresenterProtocol]{return [ControllerRoutingPresenterProtocol]()}
+    func addRoutingObserver(observer:RoutingObserverProtocol){}
+    func removeRoutingObserver(observer:RoutingObserverProtocol) -> Bool{return true}
+    func routingObserver()->[RoutingObserverProtocol]{return [RoutingObserverProtocol]()}
+}
